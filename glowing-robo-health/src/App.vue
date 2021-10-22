5bd670a33bccf0fdd80010c402e2ba623988eaca
Bryan Chen <bryancwh98@gmail.com>
2021-10-22 13:53:36 +0800

    initialised Dashboard

--- glowing-robo-health/src/App.vue
+++ glowing-robo-health/src/App.vue
@@ -1,26 +1,25 @@
-<template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+<template> 
+
+<router-view/>
+
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+console.log("in App")
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
+
 }
 </script>
 
-<style>
+<style scoped>
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
