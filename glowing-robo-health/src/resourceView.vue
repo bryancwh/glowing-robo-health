c7d76558cbfec841294a6756058de7ae3f0213ce
Yong Ler <tyongler.tang@gmail.com>
2021-10-26 16:04:55 +0800

    create page

---
+++ glowing-robo-health/src/resourceView.vue
@@ -0,0 +1,25 @@
+/<template>
+  <ResourceTable/>
+</template>
+
+<script>
+import ResourceTable from '@/components/resource_table.vue'
+
+export default {
+  name: 'App',
+  components: {
+    ResourceTable,
+  }
+}
+</script>
+
+<style scoped>
+
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+</style>
