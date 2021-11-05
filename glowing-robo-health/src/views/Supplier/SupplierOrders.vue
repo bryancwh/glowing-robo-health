433b10059cd487ee774686b3665b7168abbebb8a
zermaineeee <60999170+zermaineeee@users.noreply.github.com>
2021-11-06 03:32:01 +0800

    Positioning of NavBar buttons

--- glowing-robo-health/src/views/Supplier/SupplierOrders.vue
+++ glowing-robo-health/src/views/Supplier/SupplierOrders.vue
@@ -24,16 +24,16 @@ export default {
   },
   data() {
     return {
-      user : {
+      user: {
         email: "",
         displayName: "",
-        uid: ""
-      }
+        uid: "",
+      },
     };
   },
   mounted() {
     const auth = getAuth();
-    this.user = auth.currentUser
+    this.user = auth.currentUser;
     onAuthStateChanged(auth, (user) => {
       if (user) {
         this.user = auth.currentUser;
@@ -47,4 +47,54 @@ export default {
 };
 </script>
 
-<style></style>
+<style>
+.app_name {
+  margin: 7px 500px 0px 0px;
+  font-family: New Century Schoolbook, TeX Gyre Schola, serif;
+  font-weight: normal;
+}
+
+.topnav {
+  background-color: rgb(26, 83, 105);
+  overflow: hidden;
+  height: 70px;
+}
+
+/* Style the links inside the navigation bar */
+.topnav a {
+  float: left;
+  color: #f2f2f2;
+  text-align: right;
+  padding: 14px 16px;
+  text-decoration: none;
+  font-size: 17px;
+  margin: 10px 10px 0px 10px;
+  font-family: Comic Sans MS, Comic Sans, cursive;
+}
+
+.topnav b {
+  float: left;
+  color: #f2f2f2;
+  text-align: right;
+  padding: 14px 20px;
+  text-decoration: none;
+  font-size: 29px;
+}
+
+/* Change the color of links on hover */
+.topnav a:hover {
+  background-color: #ddd;
+  color: black;
+}
+
+/* Add a color to the active/current link */
+.topnav a.active {
+  background-color: #ddd;
+  color: black;
+}
+
+.currentLink {
+  background-color: #ddd;
+  color: black;
+}
+</style>
