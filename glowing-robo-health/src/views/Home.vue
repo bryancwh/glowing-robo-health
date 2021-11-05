01099a48a1263ac4348b1027ef33435172da60b9
Alvin Tan <e0420217@u.nus.edu>
2021-11-05 20:36:18 +0800

    Update home page to include user information

--- glowing-robo-health/src/views/Home.vue
+++ glowing-robo-health/src/views/Home.vue
@@ -1,20 +1,50 @@
 <template>
-  <div>
-    <div class="topnav">
-      <b class="app_name">Glowing Robo Health</b>
-      <a class="active" href="/">Home</a>
-      <a href="/ClinicStockpile">Clinic</a>
-      <a href="/SupplierStockpile">Supplier</a>
-      <a href="/SupplierOrders">Orders</a>
-      <a href="/ViewSupplier">Inventory</a>
-      <a href="/OrderFormTemp">Request</a>
-      <a>Logout</a>
-    </div>
+
+<div>
+  <div class="topnav">
+    <b class="app_name">Glowing Robo Health</b>
+    <a class="active" href="/">Home</a>
+    <a href="/ClinicStockpile">Clinic</a>
+    <a href="/SupplierStockpile">Supplier</a>
+    <a href="/SupplierOrders">Orders</a>
+    <a href="/ViewSupplier">Inventory</a>
+    <a href="/OrderFormTemp">Request</a>
+    <a>Logout</a>
   </div>
+
+  <h1>Welcome to glowing robo health!</h1>
+  <h1> Email: <strong>{{user.email}}</strong></h1>
+  <h1> Name: <strong>{{user.displayName}}</strong></h1>
+  <h1> Uid: <strong>{{user.uid}}</strong></h1>
+</div>
+
+
 </template>
 
 <script>
-export default {};
+
+import { getAuth, onAuthStateChanged } from "firebase/auth";
+
+export default {
+  name: 'Profile',
+  data() {
+    return {
+        user: null,
+    };
+  },
+  mounted() {
+    const auth = getAuth();
+    onAuthStateChanged(auth, (user) => {
+      if (user) {
+        this.user = user;      
+      } else {
+        console.log('not logged in')
+      }
+    })
+  }
+}
+
+
 </script>
 
 <style></style>
