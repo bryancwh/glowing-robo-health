cd6064cfc83a7567a12a6ce9228d71635c8574c5
zermaineeee <60999170+zermaineeee@users.noreply.github.com>
2021-11-14 14:13:11 +0800

    Added missing quantity column to clinic orders

--- glowing-robo-health/src/components/ClinicOrdersTable.vue
+++ glowing-robo-health/src/components/ClinicOrdersTable.vue
@@ -1,46 +1,48 @@
 <template>
   <div style="padding: 40px">
     <h1><b>Orders</b></h1>
-    <a-table
-      :columns="columns"
-      :data-source="orders"
-      bordered
-    >
-    </a-table>
+    <a-table :columns="columns" :data-source="orders" bordered> </a-table>
   </div>
 </template>
 
 <script>
 import firebaseApp from "../firebase.js";
 import { getDoc, doc, getFirestore } from "firebase/firestore";
-import {
-  collection,
-  getDocs,
-  query,
-  updateDoc,
-} from "firebase/firestore";
+import { collection, getDocs, query, updateDoc } from "firebase/firestore";
 import { getAuth, onAuthStateChanged } from "firebase/auth";
 
 const db = getFirestore(firebaseApp);
 
 const columns = [
   {
-    dataIndex: 'supplier',
-    key: 'supplier',
-    title: 'Supplier',
-    sorter: (a, b) => { return a.supplier.localeCompare(b.supplier)},
+    dataIndex: "supplier",
+    key: "supplier",
+    title: "Supplier",
+    sorter: (a, b) => {
+      return a.supplier.localeCompare(b.supplier);
+    },
   },
   {
-    title: 'Name',
-    dataIndex: 'name',
-    key: 'name',
-    sorter: (a, b) => { return a.name.localeCompare(b.name)},
+    title: "Name",
+    dataIndex: "name",
+    key: "name",
+    sorter: (a, b) => {
+      return a.name.localeCompare(b.name);
+    },
   },
   {
-    title: 'Manufacturer',
-    dataIndex: 'manufacturer',
-    key: 'manufacturer',
-    sorter: (a, b) => { return a.manufacturer.localeCompare(b.manufacturer)},
+    title: "Manufacturer",
+    dataIndex: "manufacturer",
+    key: "manufacturer",
+    sorter: (a, b) => {
+      return a.manufacturer.localeCompare(b.manufacturer);
+    },
+  },
+  {
+    title: "Quantity",
+    key: "quantity_ordered",
+    dataIndex: "quantity_ordered",
+    sorter: (a, b) => a.quantity_ordered - b.quantity_ordered,
   },
   // {
   //   title: 'Product ID',
@@ -48,45 +50,46 @@ const columns = [
   //   dataIndex: 'product_id',
   // },
   {
-    title: 'Purchase Date',
-    key: 'purchase_date',
-    dataIndex: 'purchase_date',
-     sorter: (a, b) => new Date(a.purchase_date) - new Date(b.purchase_date)
+    title: "Purchase Date",
+    key: "purchase_date",
+    dataIndex: "purchase_date",
+    sorter: (a, b) => new Date(a.purchase_date) - new Date(b.purchase_date),
   },
   {
-    title: 'Delivery Date',
-    key: 'delivery_date',
-    dataIndex: 'delivery_date',
+    title: "Delivery Date",
+    key: "delivery_date",
+    dataIndex: "delivery_date",
     sorter: (a, b) => new Date(a.delivery_date) - new Date(b.delivery_date),
     filters: [
       {
-        text: 'Not delivered',
-        value: 'Not delivered'
-      }
+        text: "Not delivered",
+        value: "Not delivered",
+      },
     ],
     onFilter: (value, record) => record.delivery_date.indexOf(value) === 0,
   },
   {
-    title: 'Status',
-    key: 'status',
-    dataIndex: 'status',
-    sorter: (a, b) => { return a.status.localeCompare(b.status)},
+    title: "Status",
+    key: "status",
+    dataIndex: "status",
+    sorter: (a, b) => {
+      return a.status.localeCompare(b.status);
+    },
     filters: [
       {
-        text: 'pending',
-        value: 'pending'
+        text: "pending",
+        value: "pending",
       },
       {
-        text: 'delivered',
-        value: 'delivered'
-      }
+        text: "delivered",
+        value: "delivered",
+      },
     ],
     onFilter: (value, record) => record.status.indexOf(value) === 0,
   },
 ];
 
 export default {
-
   mounted() {
     const auth = getAuth();
     this.user = auth.currentUser;
@@ -116,9 +119,7 @@ export default {
       return delivered == "delivered";
     },
     async display() {
-      const path = query(
-        collection(db, "orders/")
-      );
+      const path = query(collection(db, "orders/"));
 
       var email = this.user.email;
       var user_name = email.slice(0, email.indexOf("@"));
@@ -136,9 +137,17 @@ export default {
             manufacturer: data.manufacturer,
             // product_id: data.product_id,
             quantity_ordered: data.quantity_ordered,
-            purchase_date: data.purchase_date.toDate().toString().substring(3, 25),
+            purchase_date: data.purchase_date
+              .toDate()
+              .toString()
+              .substring(3, 25),
             status: data.status,
-            delivery_date: data.delivery_date ? data.delivery_date.toDate().toString().substring(4, 24) : 'Not delivered',
+            delivery_date: data.delivery_date
+              ? data.delivery_date
+                  .toDate()
+                  .toString()
+                  .substring(4, 24)
+              : "Not delivered",
             supplier: data.supplier,
           });
         }
@@ -187,11 +196,11 @@ table {
   box-shadow: none !important;
 }
 
-th span, td {
+th span,
+td {
   font-size: 14px;
 }
 th span {
   font-weight: bold !important;
 }
-
 </style>
